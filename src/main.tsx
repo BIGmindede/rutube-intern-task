af849ef728fd384a1e18ca013dce3efb3f7a1e74
Nikita Khitrov <hitrov.qazws@gmail.com>
2024-06-14 18:49:49 +0300

    RUTUBE-5: basicaly configured redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import App from 'app/App'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
+import { store } from 'shared/config/store'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 )
