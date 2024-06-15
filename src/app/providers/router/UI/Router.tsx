c99f133a7887476f9edb43f6c4f30bdaa39f744a
Nikita Khitrov <hitrov.qazws@gmail.com>
2024-06-16 01:59:43 +0300

    RUTUBE-7: fix wrap lazy loaded pages with suspense

--- src/app/providers/router/UI/Router.tsx
+++ src/app/providers/router/UI/Router.tsx
@@ -4,17 +4,20 @@ import { MainPage } from "pages/MainPage"
 import { ExtendedPage } from "pages/ExtendedPage"
 import { ThanksPage } from "pages/ThanksPage"
 import { AlreadyPassedPage } from "pages/AlreadyPassedPage"
-import React from "react"
+import React, { Suspense } from "react"
 
 const Router: React.FC = () => {
   return (
-    <Routes>
-        <Route path="/" element={<MainPage/>}/>
-        <Route path={AppRoutes.MAIN} element={<MainPage/>}/>
-        <Route path={AppRoutes.EXTENDED} element={<ExtendedPage/>}/>
-        <Route path={AppRoutes.THANKS} element={<ThanksPage/>}/>
-        <Route path={AppRoutes.ALREADY_PASSED} element={<AlreadyPassedPage/>}/>
-    </Routes>
+    
+    <Suspense>
+        <Routes>
+            <Route path="/" element={<MainPage/>}/>
+            <Route path={AppRoutes.MAIN} element={<MainPage/>}/>
+            <Route path={AppRoutes.EXTENDED} element={<ExtendedPage/>}/>
+            <Route path={AppRoutes.THANKS} element={<ThanksPage/>}/>
+            <Route path={AppRoutes.ALREADY_PASSED} element={<AlreadyPassedPage/>}/>
+        </Routes>
+    </Suspense>
   )
 }
 
