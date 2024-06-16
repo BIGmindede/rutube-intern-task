06572a37bfb973ce1fd307aed7568c22ead82a68
Nikita Khitrov <hitrov.qazws@gmail.com>
2024-06-16 08:48:05 +0300

    RUTUBE-7: completed already passed page

--- src/pages/AlreadyPassedPage/UI/AlreadyPassedPage.tsx
+++ src/pages/AlreadyPassedPage/UI/AlreadyPassedPage.tsx
@@ -1,9 +1,28 @@
+import { AppRoutes } from "app/providers/router/config/routerConfig"
 import React from "react"
+import { ButtonThemes } from "shared/UI/Button/Button"
+import { CaptionThemes } from "shared/UI/Caption/Caption"
+import Image from "shared/UI/Image/Image"
+import Form from "widgets/Form/Form"
 
 const AlreadyPassedPage: React.FC = () => {
   return (
     <main>
-        
+        <section>
+            <Image pathToFile="already_passed.png" maxWidth={524} maxHeight={232}/>
+            <Form 
+                heading="Вы уже прошли этот опрос"
+                caption={{
+                    text:"Спасибо, что делитесь мнением и помогаете нам быть лучше",
+                    theme: CaptionThemes.BASIC
+                }}
+                redirectOnComplete={AppRoutes.RUTUBE}
+                completionButton={{
+                    label: "Перейти на RUTUBE",
+                    theme: ButtonThemes.FIXED  
+                }}
+            />
+        </section>
     </main>
   )
 }
